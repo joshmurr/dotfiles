47ea0cc4b91d6607e662c25941082e09c3987552
Josh Murr <joshuadavidmurr@gmail.com>
2022-03-27 11:12:34 +0100

    Prevent newline comments workaround

--- nvim/lua/josh/options.lua
+++ nvim/lua/josh/options.lua
@@ -26,3 +26,24 @@ for k, v in pairs(options) do
 	vim.opt[k] = v
 end
 
+-- Text behaviour
+-- vim.opt.formatoptions:get()
+--  + 't'    -- auto-wrap text using textwidth
+--  + 'c'    -- auto-wrap comments using textwidth
+--  + 'r'    -- auto insert comment leader on pressing enter
+--  - 'o'    -- don't insert comment leader on pressing o
+--  + 'q'    -- format comments with gq
+--  - 'a'    -- don't autoformat the paragraphs (use some formatter instead)
+--  + 'n'    -- autoformat numbered list
+--  - '2'    -- I am a programmer and not a writer
+--  + 'j'    -- Join comments smartly
+
+-- vim.cmd [[set formatoptions-=cro]] -- This does not work
+-- see `:verbose set fortmatoptions` to see where fo is being overridden
+-- Workaround:
+vim.cmd [[
+	augroup prevent_newline_comment
+		autocmd!
+		autocmd BufEnter * set formatoptions-=cro
+	augroup end
+]]

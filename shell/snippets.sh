46529a60c86032cfe3727f9e511449a3a6adcfde
Josh Murr <joshuadavidmurr@gmail.com>
2022-04-05 15:20:58 +0100

    Some more snippets

--- shell/snippets.sh
+++ shell/snippets.sh
@@ -1,2 +1,8 @@
 # Add packages to Poetry from requirements.txt
 cat requirements.txt | xargs poetry add
+# Create video from PNGs
+ffmpeg -r 25 -f image2 -s 1024x1024 -start_number 2 -i image0000-step%*.png -vcodec libx264 -crf 25  -pix_fmt yuv420p my_face.mp4
+
+# Restart USB Ports
+sudo dpkg-reconfigure xserver-xorg
+

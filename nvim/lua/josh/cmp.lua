f3397d9cfa67446a27f79bcacfa461c6db753642
Josh Murr <joshuadavidmurr@gmail.com>
2022-04-20 10:27:15 +0100

    Update documentation thing

--- nvim/lua/josh/cmp.lua
+++ nvim/lua/josh/cmp.lua
@@ -94,7 +94,9 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = true,
+	window = {
+		documentation = "native",
+	},
 	experimental = {
 		ghost_text = false,
 		native_menu = false,

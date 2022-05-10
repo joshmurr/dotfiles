ae05a8586e7be1c79322000c0d06fd086047110f
Josh Murr <joshuadavidmurr@gmail.com>
2022-05-10 21:26:42 +0100

    Kitty Nerd font

--- nvim/lua/josh/telescope.lua
+++ nvim/lua/josh/telescope.lua
@@ -112,7 +112,7 @@ telescope.setup({
 		-- Now the picker_config_key will be applied every time you call this
 		-- builtin picker
 		find_files = {
-			disable_devicons = true,
+			disable_devicons = false,
 			hidden = true,
 		},
 	},
